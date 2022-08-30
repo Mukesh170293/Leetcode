9367ecee6ad9d41d4b46fa8365c4f48102ebe4d6
Mukesh170293 <mukesh.v.saripella@gmail.com>
2022-08-29 23:53:00 -0500

    Added problems

---
+++ String/countandsay.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        if n == 1:
+            return "1"
+        prev = self.countAndSay(n-1)
+        result = ''
+        count = 1
+        for i in range(len(prev)):
+            if i==len(prev)-1 or prev[i] != prev[i+1]:
+                result += str(count) + prev[i]
+                count = 1
+            else:
+                count += 1
+        return result
+
+print(Solution().countAndSay(5))
